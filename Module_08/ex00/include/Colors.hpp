f05e66243282dec3368f8a7f97fcd31080c63309
N0fish <allagultseva@gmail.com>
2025-03-17 17:18:30 +0100

    preparation of module 8

---
+++ Module_08/ex00/include/Colors.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Colors.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: algultse <algultse@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/17 15:30:31 by algultse          #+#    #+#             */
+/*   Updated: 2025/03/17 15:44:01 by algultse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLORS_HPP
+# define COLORS_HPP
+
+# include <string>
+# include <iostream>
+
+# define RESET "\e[0m"
+
+# define BOLD "\e[1m"
+# define DIM "\e[2m"
+# define ITALIC "\e[3m"
+# define UNDERLINE "\e[4m"
+# define BLKS "\e[5m"
+# define BLKF "\e[6m"
+# define INVERSE "\e[7m"
+# define BARR "\e[9m"
+
+# define BLACK "\e[30m"
+# define RED "\e[31m"
+# define GREEN "\e[32m"
+# define YELLOW "\e[33m"
+# define BLUE "\e[34m"
+# define PURPLE "\e[35m"
+# define CYAN "\e[36m"
+# define GRAY "\e[37m"
+
+#endif
